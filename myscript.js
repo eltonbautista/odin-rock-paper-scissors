0f48a932544bdaef41fcf8a3705442d319bd9e86
Elton Bautista <eltonbestaha@gmail.com>
2021-12-11 09:27:26 -0700

    Figured out how to make playerSelection input all lower case

--- myscript.js
+++ myscript.js
@@ -2,7 +2,8 @@
 // const playerSelection = prompt('Please enter a selection of Rock, Paper, or Scissor.')
 // playerSelection.toLowerCase;
 // Figure out how to make it so that the computer knows Rock > Scissor , Scissor > Paper, Paper > Rock. 
-/*const rock = 'rock';
+/* It looks like I was just over complicating things here by trying to add a prompt to it. Will create that in the future. 
+const rock = 'rock';
 const paper = 'paper';
 const scissor = 'scissor';
 
@@ -22,7 +23,7 @@ let paperLength = paper.length; // 5
 let scissorLength = scissor.length; // 7
 */
 
-// Have the computer randomly throw out Rock, Paper, or Scissor, we will call this function computerPlay
+// Have the computer randomly throw out Rock, Paper, or Scissor, we will call this function computerPlay. Done.
 
 function computerPlay() {
     let test = 3;
@@ -40,45 +41,34 @@ function computerPlay() {
 } 
 
 //Have the user's input go against the computer's random choice and decide on a winner, this will be done through a function called playRound(),
-//which will have two paramaters: playerSelection and computerSelection 
+//which will have two paramaters: playerSelection and computerSelection. Done, now I have to make playerSelection case insensitive.
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'rock' && computerSelection == 'scissor') {
+    switch(true) {
+        case (playerSelection == 'rock' && computerSelection == 'scissor'):
         return('Rock beats Scissor! You win!');
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return('Rock loses to Scissor. You lose!');
-    } else if (playerSelection == 'rock' && computerSelection == 'rock') {
-        return('Rock ties rock. It\'s a draw!');
+        case (playerSelection == 'rock' && computerSelection == 'paper'):
+        return('Rock loses to Paper. You lose!');
+        case playerSelection == 'rock' && computerSelection == 'rock':
+        return('Rock ties with Rock. It\'s a tie!');
+        case (playerSelection == 'paper' && computerSelection == 'rock'):
+        return('Paper beats Rock! You win!');
+        case (playerSelection == 'paper' && computerSelection == 'scissor'):
+        return('Paper loses to Scissor! You lose!');
+        case (playerSelection == 'paper' && computerSelection == 'paper'):
+        return('Paper ties with Paper. It\'s a tie!');
+        case (playerSelection == 'scissor' && computerSelection == 'paper'):
+        return('Scissor beats Paper! You win!');
+        case (playerSelection == 'scissor' && computerSelection == 'rock'):
+        return('Scissor loses to Rock! You lose!');
+        case (playerSelection == 'scissor' && computerSelection == 'scissor'):
+        return('Scissor ties with Scissor. It\'s a tie!');
     }
-    if 
 }
-   /* switch(true) {
-       case (playerSelection == 'rock' && computerSelection == 'scissor'):
-       return('Rock beats Scissor! You win!');
-       case (playerSelection == 'rock' && computerSelection == 'paper'):
-       return('Rock loses to Paper. You lose!');
-       case playerSelection == 'rock' && computerSelection == 'rock':
-       return('Rock ties with Rock. It\'s a tie!');
-       case (playerSelection == 'paper' && computerSelection == 'rock'):
-       return('Paper beats Rock! You win!');
-       case (playerSelection == 'paper' && computerSelection == 'scissor'):
-       return('Paper loses to Scissor! You lose!');
-       case (playerSelection == 'paper' && computerSelection == 'paper'):
-       return('Paper ties with Paper. It\'s a tie!');
-       case (playerSelection == 'scissor' && computerSelection == 'paper'):
-       return('Scissor beats Paper! You win!');
-       case (playerSelection == 'scissor' && computerSelection == 'rock'):
-       return('Scissor loses to Rock! You lose!');
-       case (playerSelection == 'scissor' && computerSelection == 'scissor'):
-       return('Scissor ties with Scissor. It\'s a tie!');
-   }
-   
-   
-   */
 
-
-const playerSelection = "rock";
+const playerSelection = "SCISSOR".toLowerCase(); // I only used lowercase strings in all of my input and output of 'rock', 'paper', 'scissor' so I just needed to make sure my input here is all lowercase.
 const computerSelection = computerPlay();
-// Display a message declaring who the winner of the round is. 
+console.log(playRound(playerSelection,computerSelection));
+// Display a message declaring who the winner of the round is. Done.
 
 // Create a new function called game() where we can run the RPS game 5 times and have it keep track of the score and declare a winner at the end.
